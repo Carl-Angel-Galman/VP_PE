61280b89fbb6c570a916194425f0857cf178f978
Carl Angel Galman <carl_angel.galman@mercedes-benz.com>
2026-02-28 13:01:28 +0100

    Delete obj directory

--- obj/GasSensor.d
+++
@@ -1,2 +0,0 @@
-obj/GasSensor.o: src/Service/GasSensor.c src/Service/GasSensor.h
-src/Service/GasSensor.h:
--- obj/StateMachine.d
+++
@@ -1,2 +0,0 @@
-obj/StateMachine.o: src/OS/StateMachine.c src/OS/StateMachine.h
-src/OS/StateMachine.h:
