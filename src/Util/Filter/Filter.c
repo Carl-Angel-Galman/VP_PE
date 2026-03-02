03eff137dc216e7c6679a417221b092bd77fd514
Rosenquarz26 <henrithoenejette@gmail.com>
2026-03-02 06:54:03 -0500

    added gas sensor

--- src/Util/Filter/Filter.c
+++ src/Util/Filter/Filter.c
@@ -35,7 +35,19 @@
 
 int32_t filterInitEMA(EMAFilterData_t* pEMA, int32_t scalingFactor, int32_t alpha, bool resetFilter)
 {
-    return 0;
+	if(pEMA == NULL)
+		{
+			return FILTER_ERR_INVALID_PTR;
+		}
+	if(scalingFactor = 0)
+	{
+		return FILTER_ERR_INVALID_PARAM;
+	}
+	pEMA->scalingFactor = scalingFactor;
+	pEMA->alpha = alpha;
+	pEMA->firstValueAvailable  = false;
+
+    return FILTER_ERR_OK;
 }
 
 int32_t filterResetEMA(EMAFilterData_t* pEMA)
@@ -45,5 +57,18 @@ int32_t filterResetEMA(EMAFilterData_t* pEMA)
 
 int32_t filterEMA(EMAFilterData_t* pEMA, int32_t sensorValue)
 {
-    return 0;
+	if(pEMA == NULL)
+	{
+		return FILTER_ERR_INVALID_PTR;
+	}
+	//First Sensorvalue
+	if(pEMA->firstValueAvailable == false)
+	{
+		pEMA->previousValue = sensorValue;
+		pEMA->firstValueAvailable = true;
+		return sensorValue;
+	}
+	//Calculate new Valute with EMA Filterin
+	int32_t newValue = (int32_t)(pEMA->alpha * sensorValue + (pEMA->scalingFactor-pEMA->alpha)* pEMA->previousValue)/pEMA->scalingFactor;
+    return newValue;
 }
